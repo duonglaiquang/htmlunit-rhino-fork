ab0f06488146f99679f262e20aa1ac570b6e2a39
Marc Guillemot <mguillemot@yahoo.fr>
2012-11-13 14:21:33 +0100

    avoid that function properties arguments and caller get enumerated

--- src/org/mozilla/javascript/Interpreter.java
+++ src/org/mozilla/javascript/Interpreter.java
@@ -2867,6 +2867,7 @@ switch (op) {
     {
        if (frame.parentFrame != null && !frame.parentFrame.fnOrScript.isScript()) {
            frame.fnOrScript.defaultPut("caller", frame.parentFrame.fnOrScript);
+           frame.fnOrScript.setAttributes("caller", ScriptableObject.DONTENUM);
        }
        Object[] parameters = null;
        if (frame.scope instanceof NativeCall) {
@@ -2879,7 +2880,7 @@ switch (op) {
                System.arraycopy(args, args.length - parameters.length, parameters, 0, parameters.length);
            }
        }
-       frame.fnOrScript.defaultPut("arguments", new Arguments(parameters));
+       frame.fnOrScript.setArguments(new Arguments(parameters));
 
         boolean usesActivation = frame.idata.itsNeedsActivation;
         boolean isDebugged = frame.debuggerFrame != null;
@@ -2931,7 +2932,7 @@ switch (op) {
                                   Object throwable)
     {
         frame.fnOrScript.defaultPut("caller", null);
-        frame.fnOrScript.defaultPut("arguments", null);
+        frame.fnOrScript.setArguments(null);
 
         if (frame.idata.itsNeedsActivation) {
             ScriptRuntime.exitActivationFunction(cx);
