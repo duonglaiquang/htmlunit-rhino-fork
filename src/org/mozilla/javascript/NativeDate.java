1ab7b9980f35cce6d268b3c889efdf1c6a15ca88
Ahmed Ashour <asashour@yahoo.com>
2012-12-07 06:34:57 +0300

    NativeDate: fix .toLocaleDateString()

--- src/org/mozilla/javascript/NativeDate.java
+++ src/org/mozilla/javascript/NativeDate.java
@@ -1270,7 +1270,7 @@ final class NativeDate extends IdScriptableObject
           case Id_toLocaleDateString:
             if (localeDateFormatter == null) {
                 localeDateFormatter
-                    = DateFormat.getDateInstance(DateFormat.LONG);
+                    = new SimpleDateFormat("EEEE, MMMM dd, yyyy");
             }
             formatter = localeDateFormatter;
             break;
