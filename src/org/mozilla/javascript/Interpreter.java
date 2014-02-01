6989551df64dd03f90461e0111d07628ab7d42b1
Ahmed Ashour <asashour@yahoo.com>
2014-02-01 14:08:42 +0300

    Fix 'arguments' if specified in the parameter list.

--- src/org/mozilla/javascript/Interpreter.java
+++ src/org/mozilla/javascript/Interpreter.java
@@ -2881,7 +2881,10 @@ switch (op) {
            frame.fnOrScript.setAttributes("caller", ScriptableObject.DONTENUM);
        }
        if (frame.scope instanceof NativeCall) {
-           frame.fnOrScript.setArguments((Arguments) ScriptableObject.getProperty(frame.scope, "arguments"));
+           Object arguments = ScriptableObject.getProperty(frame.scope, "arguments");
+           if (arguments instanceof Arguments) {
+               frame.fnOrScript.setArguments((Arguments) arguments);
+           }
        }
 
         boolean usesActivation = frame.idata.itsNeedsActivation;
