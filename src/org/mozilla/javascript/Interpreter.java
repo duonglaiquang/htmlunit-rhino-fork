c2f8f8c550b4f861d04d9b85395b10b0900ba93b
Ahmed Ashour <asashour@yahoo.com>
2014-02-03 11:48:24 +0300

    fix build

--- src/org/mozilla/javascript/Interpreter.java
+++ src/org/mozilla/javascript/Interpreter.java
@@ -2885,7 +2885,6 @@ switch (op) {
            if (arguments instanceof Arguments) {
                frame.fnOrScript.setArguments((Arguments) arguments);
            }
-           frame.fnOrScript.setArguments((Arguments) ScriptableObject.getProperty(frame.scope, "arguments"));
        }
 
         boolean usesActivation = frame.idata.itsNeedsActivation;
