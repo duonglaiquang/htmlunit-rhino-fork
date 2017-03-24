656c465b80aa6b78873bc869a25ce39ff63eb941
Ahmed Ashour <asashour@yahoo.com>
2017-03-24 11:02:22 +0100

    Fix Object.getPrototypeOf

--- src/org/mozilla/javascript/NativeObject.java
+++ src/org/mozilla/javascript/NativeObject.java
@@ -272,6 +272,15 @@ public class NativeObject extends IdScriptableObject implements Map
           case ConstructorId_getPrototypeOf:
               {
                 Object arg = args.length < 1 ? Undefined.instance : args[0];
+                if (arg instanceof String) {
+                    return "";
+                }
+                else if (arg instanceof Number) {
+                    return 0;
+                }
+                else if (arg instanceof Boolean) {
+                    return false;
+                }
                 Scriptable obj = ensureScriptable(arg);
                 return obj.getPrototype();
               }
